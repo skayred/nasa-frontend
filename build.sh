8bb13c88530f9e9b79fe69442fb3948748df34fb
Dmitrii Savchenko <dg.freak@gmail.com>
2022-11-29 17:42:34 +0200

    [master] Link alive

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 npm run build
 
-docker build . -t registry.panim.one/nasa-frontend:0.0.1 -f ./Dockerfile --progress=plain --no-cache
+docker build . -t registry.panim.one/nasa-frontend:0.0.2 -f ./Dockerfile --progress=plain --no-cache
 
 npm clean
